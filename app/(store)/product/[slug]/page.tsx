da622d1dc5aa0712f8de3ac40ee03c21b59f4ac9
abduljalal-s <nightmadecoder@gmail.com>
2025-04-12 15:50:10 +0100

    checkoutSession and stripe implementation

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -47,7 +47,7 @@ async function ProductPage({
                     <div>
                         <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
                         <div className="text-xl font-semibold mb-4">
-                            N{product.price?.toFixed(2)}
+                            ₦{product.price?.toFixed(2)}
                         </div>
                         <div className="prose max-w-none mb-6">
                             {Array.isArray(product.description) && (
